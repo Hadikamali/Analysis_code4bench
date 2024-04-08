142ae453fc021c445accc188a881760d1b730b43
Mohammad Hadi Kamali <hadi.appleidcard@gmail.com>
2024-04-08 18:10:38 +0330

    Add files via upload

---
+++ Code4Bench-master/scripts/java/crawlContext.java
@@ -0,0 +1,22 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.lang.reflect.Executable;
+import java.math.RoundingMode;
+import java.sql.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class problemnumber {
+    public static ResultSet rs = null;
+    public static Connection con = null;
+    public static boolean flagDB = true;
+
+    public static void main(String[] args) throws Exception {
+    }
+}
+
+
